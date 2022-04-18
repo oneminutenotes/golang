fe9b719f98092d24d58583f3d91394ef8e73fe56
surajresearch <oneminutenotes.channel@gmail.com>
2022-04-18 10:25:37 +0530

    finding duplicate lines

---
+++ duplines/dup1.go
@@ -0,0 +1,28 @@
+package main
+// this program prints lines which appear more than
+// once on a standard output and their count
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		if input.Text() == "" {
+			break
+		}
+		counts[input.Text()]++
+	}
+
+	// Note: ignoring potential errors from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
---
+++ duplines/dup2.go
@@ -0,0 +1,44 @@
+// this program prints count & text of repeated lines
+// it accepts inputs from stdin or from list of files
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		if input.Text() == "" {
+			break
+		}
+		counts[input.Text()]++
+	}
+	// Note: ignoring potential error from input.Err()
+}
